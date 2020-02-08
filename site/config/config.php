8da3a0fe7127fcd99b924f4037cb6059102b62c9
Johann Schopplich <hello@johanns.design>
2020-02-08 16:15:18 +0100

    Blueprint cache option cache (if enabled globally)

--- site/config/config.php
+++ site/config/config.php
@@ -8,10 +8,21 @@ return [
     'debug' => env('KIRBY_DEBUG', false),
 
     'panel' => [
-        'install' => env('KIRBY_INSTALL', false)
+        'install' => env('KIRBY_PANEL_INSTALL', false),
+        'slug' => env('KIRBY_PANEL_SLUG', 'panel')
     ],
 
     'routes' => require_once 'routes.php',
+
+    'cache' => [
+        'pages' => [
+            'active' => env('KIRBY_CACHE', false),
+            'ignore' => function ($page) {
+                $options = $page->blueprint()->options();
+                return isset($options['cache']) ? !$options['cache'] : false;
+            }
+        ]
+    ],
 
     'thumbs' => [
         'quality' => '80',
