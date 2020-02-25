883d0da7decdeec28208cb5c5410a9d94856e0a3
Johann Schopplich <hello@johanns.design>
2020-02-25 14:06:05 +0100

    Remove JWT test leftovers

--- frontend/src/api/ky.js
+++ frontend/src/api/ky.js
@@ -1,13 +1,10 @@
 import Ky from 'ky'
 
-// const token = localStorage.getItem('jwt')
-
 const ky = Ky.extend({
   hooks: {
     beforeRequest: [
       request => {
         request.headers.set('X-Requested-With', 'ky')
-        // if (token) request.headers.set('Authorization', `Bearer ${token}`)
       }
     ]
   }
