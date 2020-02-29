1860aa8a064bc15f94dad6440f38031d007e7da9
Johann Schopplich <hello@johanns.design>
2020-02-29 15:08:47 +0100

    Extract ssg meta tags into Kirby snippet

--- site/controllers/site.php
+++ site/controllers/site.php
@@ -1,7 +1,7 @@
 <?php
 
 return function ($page, $site) {
-    $pageTitle = (!$page->isHomePage() ? $page->title() . ' – ' : '') . $site->title();
+    $pageTitle = $page->customTitle()->or($page->title() . ' – ' . $site->title());
     $pageDescription = $page->description()->or($site->description());
     $siteThumbnail = $site->thumbnail()->toFile() ? $site->thumbnail()->toFile()->url() : null;
     $pageThumbnail = $page->thumbnail()->toFile() ? $page->thumbnail()->toFile()->url() : $siteThumbnail;
---
+++ site/snippets/meta.php
@@ -0,0 +1,30 @@
+<title><?= $pageTitle ?></title>
+<meta name="description" content="<?= $pageDescription ?>">
+<link rel="canonical" href="<?= $page->url() ?>">
+
+<meta property="og:type" content="website">
+<meta property="og:url" content="<?= $page->url() ?>">
+<meta property="og:title" content="<?= $pageTitle ?>">
+<meta property="og:description" content="<?= $pageDescription ?>">
+<meta property="og:image" content="<?= $pageThumbnail ?>">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:domain" content="<?= Url::host() ?>">
+<meta name="twitter:title" content="<?= $pageTitle ?>">
+<meta name="twitter:description" content="<?= $pageDescription ?>">
+<meta name="twitter:image" content="<?= $pageThumbnail ?>">
+<meta name="twitter:url" content="<?= $page->url() ?>">
+
+<?php /*
+<meta name="application-name" content="<?= $site->title() ?>">
+<meta name="theme-color" content="#333333">
+
+<link rel="manifest" href="<?= url('manifest.json') ?>">
+<link rel="icon" href="<?= url('img/favicon-32x32.png') ?>" sizes="32x32" type="image/png">
+<link rel="icon" href="<?= url('img/favicon-16x16.png') ?>" sizes="16x16" type="image/png">
+<link rel="apple-touch-icon" href="<?= url('img/apple-touch-icon.png') ?>" sizes="180x180">
+
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<meta name="apple-mobile-web-app-title" content="<?= $site->title() ?>">
+*/ ?>
