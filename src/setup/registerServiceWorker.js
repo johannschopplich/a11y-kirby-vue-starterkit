e308a580b9558fe804fd4fe84e86131cddcbcfd7
Johann Schopplich <mail@johannschopplich.com>
2020-04-14 00:00:30 +0200

    Fix service worker pre-caching media files

--- src/setup/registerServiceWorker.js
+++ src/setup/registerServiceWorker.js
@@ -1,17 +1,27 @@
 import { Workbox } from 'workbox-window'
 
-let wb
+let wb = null
 
 if ('serviceWorker' in navigator) {
   wb = new Workbox(`${process.env.BASE_URL}service-worker.js`)
+
+  wb.addEventListener('activated', event => {
+    // `event.isUpdate` will be true if another version of the service
+    // worker was controlling the page when this version was registered.
+    if (!event.isUpdate) {
+      console.log('Service worker activated for the first time!')
+    }
+  })
+
+  wb.addEventListener('waiting', () => {
+    console.log('A new service worker has installed, but it can\'t activate until all tabs running the current version have fully unloaded.')
+  })
 
   wb.addEventListener('controlling', () => {
     window.location.reload()
   })
 
   wb.register()
-} else {
-  wb = null
 }
 
 export default wb
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const { GenerateSW } = require('workbox-webpack-plugin')
 const config = require('./kirby.config')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 process.env.VUE_APP_API_URL = process.env.NODE_ENV === 'production' ? config.prodApi : config.devApi
 
@@ -28,20 +28,35 @@ module.exports = {
     }
   },
 
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       implementation: require('sass'),
-  //       prependData: `
-  //         @import "@/styles/_variables.scss";
-  //       `
-  //     }
-  //   }
-  // },
-
   configureWebpack: {
     plugins: [
-      new GenerateSW()
+      new WorkboxPlugin.GenerateSW({
+        exclude: [/\.(?:json|png|jpg|jpeg|svg|ico|htaccess)$/, /index\..*/, 'media'],
+
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 30
+              }
+            }
+          },
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'api',
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 7 // 7 days
+              }
+            }
+          }
+        ]
+      })
     ]
   }
 }
