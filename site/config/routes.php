e1300a13d75756cc96c84f0e5da96a963da3c71f
Johann Schopplich <mail@johannschopplich.com>
2020-04-14 12:10:10 +0200

    Fix Kirby response for index.html

--- site/config/routes.php
+++ site/config/routes.php
@@ -31,7 +31,8 @@ return [
             // Enable CORS for Vue development environment
             if (option('debug') === true) header('Access-Control-Allow-Origin: *');
 
-            if (empty($pageId) || ($pageId === 'index.html')) $pageId = $site->homePage()->id();
+            if (empty($pageId)) $pageId = $site->homePage()->id();
+            if ($pageId === 'index.html') $pageId = $site->homePage()->id();
             $page = page($pageId) ?? page('error');
 
             if (get('content') === 'json') {
